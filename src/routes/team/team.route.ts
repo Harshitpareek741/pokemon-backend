5842efb1f60e09179d525f09e34248c42be7f8b0
Harshitpareek741 <harshitpareek132@gmail.com>
2025-03-22 09:30:14 +0530

    thrid

--- src/routes/team/team.route.ts
+++ src/routes/team/team.route.ts
@@ -1,15 +1,17 @@
 import express from 'express';
 import TeamController from '../../controllers/team/teamcontroller.js';
+import { isAuthenticated } from '../../middlewares/isAuthenticated.js';
+
 
 
 const router = express.Router();
 
-router.post('/', (req, res) => TeamController.create(req, res));
-router.post('/addmember', (req, res) => TeamController.addMember(req, res));
-router.get('/:id', (req, res) => TeamController.get(req, res));
+router.post('/', isAuthenticated("user&admin") , (req, res) => TeamController.create(req, res));
+router.post('/addmember', isAuthenticated("user&admin"),(req, res) => TeamController.addMember(req, res));
+router.get('/:id', isAuthenticated("user&admin"),(req, res) => TeamController.get(req, res));
 router.get('/', (req, res) => TeamController.getAll(req, res));
-router.put('/:id', (req, res) => TeamController.update(req, res));
-router.delete('/:id', (req, res) => TeamController.delete(req, res));
+router.put('/:id', isAuthenticated("user&admin"), (req, res) => TeamController.update(req, res));
+router.delete('/:id', isAuthenticated("user&admin"), (req, res) => TeamController.delete(req, res));
 router.get('/:id/stats', (req, res) => TeamController.stats(req, res));
 
 export default router;
